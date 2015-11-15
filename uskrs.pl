a7edbd270bd25931ceeec3dadd896ae92e90c4e0
Matija Nalis <mnalis-git@voyager.hr>
2015-11-15 02:06:46 +0100

    use Date::Calc instead of manual calculations; add Tjelovo i Uskrsnji ponedjeljak

--- uskrs.pl
+++ uskrs.pl
@@ -6,7 +6,11 @@ use strict;
 use warnings;
 use autodie;
 
-my $VERSION='0.1';
+use Date::Calc qw(Easter_Sunday Add_Delta_Days);
+
+my $VERSION='0.2';
+my $year_start = 2015;
+my $year_end = 2099;
 
 # returns current time in YYYYMMDD T HHMMSS Z format
 sub time_now()
@@ -28,7 +32,7 @@ CREATED:$now
 LAST-MODIFIED:$now
 DTSTAMP:$now
 UID:$e_name-$e_year
-SUMMARY:$e_name $e_year
+SUMMARY:$e_name $e_year (neradni)
 DTSTART;VALUE=DATE:$event_date
 DTEND;VALUE=DATE:$event_date
 END:VEVENT
@@ -36,33 +40,24 @@ END:VEVENT
 EOL
 }  
   
-sub one_easter($$$)
-{
-  return one_event ('Uskrs', @_);
-}
+sub one_easter($$$)	{ return one_event ('Uskrs', @_) }
+sub one_eas_mon($$$)	{ return one_event ('Uskršnji pon.', Add_Delta_Days(@_, 1)) }
+sub one_tjelovo($$$)	{ return one_event ('Tjelovo', Add_Delta_Days(@_, 9*7-3)) }
 
 # iCal header
 print <<EOF;
 BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//voyager.hr//NONSGML uskrs.pl $VERSION//HR
-X-WR-CALNAME:Uskrsi
+X-WR-CALNAME:Uskrs i tjelovo
 EOF
 
 # modify years range as needed in line below
-foreach my $year (2015..2099) {
-  # calculate Easter date. Voodoo magic calculations - see "2.9.6. Isn't there a simpler way to calculate Easter?" from https://web.archive.org/web/19970628070604/http://www.math.uio.no/faq/calendars/faq.html
-  my $century = int($year/100);
-  my $G = $year % 19;
-  my $K = int(($century - 17)/25);
-  my $I = ($century - int($century/4) - int(($century - $K)/3) + 19*$G + 15) % 30;
-  $I = $I - int($I/28)*(1 - int($I/28)*int(29/($I + 1))*int((21 - $G)/11));
-  my $J = ($year + int($year/4) + $I + 2 - $century + int($century/4)) % 7;
-  my $L = $I - $J;
-  my $EasterMonth = 3 + int(($L + 40)/44);
-  my $EasterDay = $L + 28 - 31*int($EasterMonth/4);
-  
-  one_easter($year, $EasterMonth, $EasterDay);
+foreach my $year ($year_start..$year_end) {
+    my ($year, $EasterMonth, $EasterDay) = Easter_Sunday($year);
+    one_easter ($year, $EasterMonth, $EasterDay);	# uskrs (Easter)
+    one_eas_mon($year, $EasterMonth, $EasterDay);  	# uskrsnji pon. (monday after Easter)
+    one_tjelovo($year, $EasterMonth, $EasterDay);  	# Tjelovo (Corpus Christi)
 }
 
 # iCal footer
